70c29c7b3082cd13b254b63dd0c866f5e3936b53
polishchlieb <biczu.yt@gmail.com>
2019-04-06 22:53:16 +0200

    Update

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bot_1 = require("./bot");
-const { token } = require('../config.json');
-exports.bot = new bot_1.Bot();
-exports.bot.start(token);
