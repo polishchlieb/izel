82c9fcc41658743597434cfa1cf47e09c6d85f09
pizza61 <34103636+pizza61@users.noreply.github.com>
2019-06-10 19:48:38 +0200

    nowplaying tweaks

--- src/commands/playing.ts
+++ src/commands/playing.ts
@@ -17,6 +17,12 @@ export default class PlayingCommand implements Command {
         if(!server)
             return message.reply(messages.nothingPlaying);
         else {
+            let length: number = Math.floor(server.dispatcher.time / 1000);
+
+            let minutes: number = Math.floor(length / 60);
+            let seconds: number = length % 60;
+            let time: string = `${minutes}:${("0" + seconds).slice(-2)}`;
+
             let { playing }: MusicServer = server;
             let respEmbed: RichEmbed = new RichEmbed()
                 .setAuthor(messages.nowPlaying, message.author.avatarURL)
@@ -24,7 +30,8 @@ export default class PlayingCommand implements Command {
                 .setThumbnail(playing.thumbnail)
                 .setTitle(playing.title)
                 .setURL(playing.link)
-                .addField(messages.queryRequested, playing.requester, true);
+                .addField(messages.queryRequested, playing.requester, true)
+                .addField(messages.playingTime, time, true)
 
             if(playing.channel) respEmbed.addField(messages.videoChannel, playing.channel, true);
             message.channel.send(respEmbed);
